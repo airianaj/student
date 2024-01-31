ec3c663a98e0f36bf5c9a383d3f13cb39e285613
airianaj <abfe.am@gmail.com>
2024-02-01 01:20:25 +0900

    テーブル作成
    students
    school_gradesのテーブル作成

---
+++ database/migrations/2024_02_01_005930_create_school_grades_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchoolGradesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('school_grades', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('student_id')->nullable();
+            $table->foreign('student_id')->references('id')->on('students');
+            $table->string('grade');
+            $table->string('term');
+            $table->string('japanese');
+            $table->string('math');
+            $table->string('science');
+            $table->string('social_studies');
+            $table->string('music');
+            $table->string('home_economics');
+            $table->string('english');
+            $table->string('art');
+            $table->string('health_and_physical_education');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('school_grades');
+    }
+}
